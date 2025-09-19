80b8f19e7b17d99ebb40e741ba61029e55820c04
dendenmushi <136006438+averageC0deEnjoyer@users.noreply.github.com>
2025-09-19 16:07:59 +0700

    wip

--- sandbox/O/1/main.cpp
+++ sandbox/O/1/main.cpp
@@ -4,11 +4,22 @@
 int main()
 {
   std::bitset<8> bits{0b0000'0101};
-  bits.set(3);
-  bits.flip(4);
-  bits.reset(4);
+  // bits.set(3);
+  // bits.flip(4);
+  // bits.reset(4);
+  //
+  // std::cout << "All the bits " << bits << '\n';
+  // std::cout << "Bits at pos 3 " << bits.test(3) << '\n';
+  // std::cout << "Bits at pos 4 " << bits.test(4) << '\n';
+  std::cout << bits.size() << " bits are in the bitset\n";
+  std::cout << bits.count() << " bits are set to true\n";
 
-  std::cout << "All the bits " << bits << '\n';
-  std::cout << "Bits at pos 3 " << bits.test(3) << '\n';
-  std::cout << "Bits at pos 4 " << bits.test(4) << '\n';
+  std::cout << std::boolalpha;
+  std::cout << "All bits are true: " << bits.all() << '\n';
+
+  std::cout << "Some bits are true: " << bits.any() << '\n';
+
+  std::cout << "No bits are true: " << bits.none() << '\n';
+
+  return 0;
 }
---
+++ sandbox/O/2/main.cpp
@@ -0,0 +1,63 @@
+// #include <bitset>
+// #include <cstdint>
+// #include <iostream>
+// #include <typeinfo>
+
+// int main()
+// {
+//   std::bitset<4> x{0b1100};
+//
+//   x <<= 1;
+//
+//   std::cout << x << '\n';
+//
+//   return 0;
+// }
+// int main()
+// {
+//   std::uint8_t c{0b00001111};
+//   std::cout << typeid(c).name() << std::endl;
+//
+//   // std::uint8_t cneg{~c};
+//   return 0;
+// }
+
+// in my own words, first shift 6, type become int, then we cut by static_cast,
+// then we romote again to bitset 32
+//  int main()
+//  {
+//    std::uint8_t c{0b00001111};
+//    std::cout << std::bitset<32>(static_cast<std::uint8_t>(c << 6)) << '\n';
+//    return 0;
+//  }
+//
+// QUESTION 2
+#include <bitset>
+#include <iostream>
+
+// "rotl" stands for "rotate left"
+std::bitset<4> rotl(std::bitset<4> bits)
+{
+  // Your code here
+  bool isLeftMost{false};
+  // check the left most bit
+  if (bits.test(3))
+    isLeftMost = true;
+  // shift left
+  bits <<= 1;
+  // set the most right bit to 1 if condition
+  if (isLeftMost)
+    bits.set(0);
+  return bits;
+}
+
+int main()
+{
+  std::bitset<4> bits1{0b0001};
+  std::cout << rotl(bits1) << '\n';
+
+  std::bitset<4> bits2{0b1001};
+  std::cout << rotl(bits2) << '\n';
+
+  return 0;
+}
